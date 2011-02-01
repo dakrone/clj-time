afd02bf6e0680662fb7cb0efe3f558d397e4a30d
Lee Hinman <lee@writequit.org>
2011-01-31 18:12:58 -0700

    Changes to project info for stable fork

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject clj-time "0.3.0-SNAPSHOT"
+(defproject org.clojars.thnetos/clj-time "0.3.0-SNAPSHOT"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [joda-time "1.6"]]
-  :repositories {"snapshots" "http://mvn.getwoven.com/repos/woven-public-snapshots"
-                 "releases" "http://mvn.getwoven.com/repos/woven-public-releases"})
+                 [joda-time "1.6.2"]])
